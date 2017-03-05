97281fcdeaf6edd0839d2b86591c0c14d258bd60
NadimRai <rai_nadim@yahoo.com>
2017-03-04 20:20:51 -0800

    created chefs table and chef model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170305040854) do
+ActiveRecord::Schema.define(version: 20170305041731) do
+
+  create_table "chefs", force: :cascade do |t|
+    t.string   "chefname"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string   "name"
